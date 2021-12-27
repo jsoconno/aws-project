946e7210b4a2813698a7be67996dbbda39963405
Justin O'Connor <justinoconnor@justins-air.lan>
2021-12-27 14:06:43 -0500

    update reference to module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "ecr" {
-    source = "github.com/jsoconno/aws-modules-ecr?ref=v1.0.0"
+    source = "github.com/jsoconno/terraform-module-aws-ecr?ref=v1.0.0"
     name = "ecr-test"
 
     tags = var.tags
