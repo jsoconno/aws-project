751f1a77ce62b04cf247181b250af19552c97302
Justin O'Connor <justinoconnor@justins-air.lan>
2021-12-28 13:28:04 -0500

    fix ref to module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "s3" {
-    source = "github.com/jsoconno/terraform-module-aws-iam?ref=v1.0.0"
+    source = "github.com/jsoconno/terraform-module-aws-s3?ref=v1.0.0"
     # source = "../terraform-module-aws-s3"
 
     tags = var.tags
