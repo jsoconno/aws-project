6bf0ee2ee510c832118dc9eca28cc406dc903aa4
Justin O'Connor <justinoconnor@justins-air.lan>
2021-12-28 13:27:19 -0500

    test

--- main.tf
+++ main.tf
@@ -1,45 +1,6 @@
-data "aws_iam_policy_document" "assume_role_policy" {
-    statement {
-        effect = "Allow"
-        actions = [
-            "sts:AssumeRole"
-        ]
-        principals {
-            type = "Service"
-            identifiers = [
-                "ec2.amazonaws.com"
-            ]
-        }
-    }
-}
-
-data "aws_iam_policy_document" "example" {
-  statement {
-    sid = "1"
-
-    actions = [
-      "s3:ListAllMyBuckets",
-      "s3:GetBucketLocation",
-    ]
-
-    resources = [
-      "arn:aws:s3:::*",
-    ]
-  }
-}
-
-module "iam" {
+module "s3" {
     source = "github.com/jsoconno/terraform-module-aws-iam?ref=v1.0.0"
-    # source = "../terraform-module-aws-iam"
-    
-    name = "test-role"
-    assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-    policies = [
-        data.aws_iam_policy_document.example.json,
-    ]
-    remote_policies = [
-        "arn:aws:iam::aws:policy/AWSDirectConnectReadOnlyAccess"
-    ]
+    # source = "../terraform-module-aws-s3"
 
     tags = var.tags
 }
