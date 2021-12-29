dfcc758a850f189cd3ec6fc08af0ce5ab06f3b36
Justin O'Connor <justinoconnor@justins-air.lan>
2021-12-29 15:53:37 -0500

    updated code for testing current modules

--- main.tf
+++ main.tf
@@ -1,11 +1,110 @@
-module "lambda" {
-  source = "github.com/jsoconno/terraform-module-aws-lambda?ref=v1.0.1"
-#   source = "../terraform-module-aws-lambda"
+data "aws_caller_identity" "current" {}
 
-  name = "test-lambda"
-  layers = [
-    "arn:aws:lambda:us-east-1:363121595583:layer:CustomLayer:1"
-  ]
+data "aws_region" "current" {}
+
+locals {
+  body = jsonencode({
+    swagger = "2.0"
+    info = {
+      title   = "Example"
+      version = "1.0"
+    }
+    schemes = [
+      "https"
+    ]
+    paths = {
+      "/path" = {
+        get = {
+          responses = {
+            "200" = {
+              description = "200 response"
+            }
+          }
+          x-amazon-apigateway-integration = {
+            type       = "AWS"
+            uri        = module.lambda.invoke_arn
+            httpMethod = "POST"
+            responses = {
+              default = {
+                statusCode = 200
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+module "api_gateway" {
+  source = "github.com/jsoconno/terraform-module-aws-api-gateway?ref=v1.0.0"
+  #   source = "../terraform-module-aws-api-gateway"
+
+  name = "test-api-gateway"
+  body = local.body
 
   tags = var.tags
+}
+
+module "lambda" {
+  source = "github.com/jsoconno/terraform-module-aws-lambda?ref=v1.1.1"
+  #   source = "../terraform-module-aws-lambda"
+
+  name = "test-lambda"
+
+  environment_variables = {
+    REGION = data.aws_region.current.name
+    BUCKET = module.s3.id
+  }
+
+  api_gateway_source_arns = [
+    module.api_gateway.execution_arn
+  ]
+
+  # Something to consider adding later
+  #   s3_bucket_target_arns = [
+
+  #   ]
+
+  tags = var.tags
+}
+
+module "s3" {
+  source = "github.com/jsoconno/terraform-module-aws-s3?ref=v1.0.0"
+
+  tags = var.tags
+}
+
+data "aws_iam_policy_document" "allow_access_s3" {
+  statement {
+    sid    = "AllowAccessToListBucket"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [
+      module.s3.arn
+    ]
+  }
+
+  statement {
+    sid    = "AllowAccessToPutObject"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${module.s3.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "allow_access_s3" {
+  name   = "${module.lambda.name}-s3-access"
+  policy = data.aws_iam_policy_document.allow_access_s3.json
+}
+
+resource "aws_iam_role_policy_attachment" "allow_access_s3" {
+  role       = module.lambda.role_name
+  policy_arn = aws_iam_policy.allow_access_s3.arn
 }
--- src/main.py
+++ src/main.py
@@ -1,4 +1,33 @@
-import layers
+import os
+import boto3
 
 def lambda_handler(event, context):
-    layers.test_function()
+    region = os.environ["REGION"]
+    bucket = os.environ["BUCKET"]
+    name = "file"
+    body = "end-to-end success"
+
+    response = upload_file(
+        region=region,
+        bucket=bucket,
+        name=name,
+        body=body
+    )
+
+    return response
+
+def upload_file(region, bucket, name, body):
+    try:
+        s3_client = boto3.client('s3', region_name=region)
+        response = s3_client.put_object(
+            Body=body,
+            Bucket=bucket,
+            Key=name,
+            ServerSideEncryption="aws:kms"
+        )
+        print(response)
+    except Exception as e:
+        response = e
+        print(response)
+    
+    return response
